e7014911057d6103f468b4391433972f10147c90
Nicolas Sebrecht <nicolas.s-dev@laposte.net>
2015-11-01 04:42:20 +0100

    controllers.Controller: improve documentation
    Signed-off-by: Nicolas Sebrecht <nicolas.s-dev@laposte.net>

--- imapfw/controllers/controller.py
+++ imapfw/controllers/controller.py
@@ -25,32 +25,33 @@
 A controller is defined on a repository to control its end-driver.
 
 Controllers have the same public interface as the drivers. Each responds like a
-driver, does its job and relays the requests (results) to (from) the underlying
-driver.
+driver, does its job and relays the requests (and results) to (or from) the
+underlying driver.
 
-Controllers can be chained to each others so that the flows is passed into
-each.
+Controllers can be chained to each others so that the flows pass through each.
 
 They can either be passive or active. Passive controllers follow-up the requests
 as-is and returns the unchanged results. Active controllers changes the flow to
 achieve their tasks. IOW, each controller can only view what the underlying
-controller accepts to show. Hence, the order is important.
+controller accepts to show. Hence, the order in the chain is important.
 
 The controller base "Controller" is a passive controller (see code below).
 
 The chain of controller is defined in the rascal.
 
 
-SCHEMATIC OVERVIEW EXAMPLE (half part)
---------------------------------------
+SCHEMATIC OVERVIEW EXAMPLE (right side)
+---------------------------------------
+
                  (filter)      (tracker)      (encoder)
++----------+   +----------+   +----------+   +----------+   +----------+
+|          |-->|          |-->|          |-->|          |-->|          |
+|  engine  |   |controller+   |controller|   |controller|   |  driver  |
+|          |<--|          |<--|          |<--|          |<--|          |
++----------+   +----------+   +----------+   +----------+   +----------+
+                 [active]       [passive]      [active]
                               notifications,  UTF-7/UTF-8
-                               debugging
-+----------+   +----------+   +----------+   +----------+   +----------+
-|          |   |          |   |          |   |          |   |          |
-|  engine  |<->|controller+<->|controller|<->|controller|<->|  driver  |
-|          |   |          |   |          |   |          |   |          |
-+----------+   +----------+   +----------+   +----------+   +----------+
+                                debugging
 
 
 """
