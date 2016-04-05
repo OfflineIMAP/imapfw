794c0f6c74627fcafca2418ec7a02dec7d9af309
Nicolas Sebrecht <nicolas.s-dev@laposte.net>
2016-04-06 01:02:08 +0200

    minor: __init__: improve infos
    Signed-off-by: Nicolas Sebrecht <nicolas.s-dev@laposte.net>

--- imapfw/__init__.py
+++ imapfw/__init__.py
@@ -4,10 +4,10 @@
 
 __productname__ = 'Imapfw'
 __version__     = "0.025"
-__copyright__   = "Copyright 2016 Nicolas Sebrecht & contributors"
+__copyright__   = "Copyright 2015-2016 Nicolas Sebrecht & contributors"
 __author__      = "Nicolas Sebrecht"
 __author_email__= "nicolas.s-dev@laposte.net"
-__description__ = "Easy IMAP framework for mail access and synchronization"
+__description__ = "Framework for working with IMAP and emails"
 __license__  = "The MIT License (MIT)"
 __homepage__ = "http://github.com/OfflineIMAP/imapfw"
 
